16fe61b5d4cc042498ae43ed27d9b60d09b7d08f
rama <rama.4ditya3@gmail.com>
2025-03-04 10:14:14 +0700

    Majelis Bikini Bottom

---
+++ Majelis-Bikini-Bottom/index.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+  anime({
+    targets: "#text",
+    width: ["0", "100%"],
+    duration: 3000,
+    easing: "easeInOutSine",
+    begin: function () {
+      document.getElementById("text").style.overflow = "hidden";
+      document.getElementById("text").style.display = "inline-block";
+    },
+    complete: function () {
+      document.getElementById("cursor").style.animation = "none";
+      setTimeout(() => {
+        document.getElementById("handwrittenText").style.display = "none";
+        const video = document.getElementById("video");
+        video.style.display = "block";
+        video.load();
+        video.play();
+        video.muted = false;
+      }, 1000);
+    },
+  });
+});
