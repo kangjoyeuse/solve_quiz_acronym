a3d6aa66c559e821296f461144b8e7fc5938a127
yohanesokta <admin@yohancloud.biz.id>
2025-03-06 12:16:00 +0700

    feat : checkbox delete

--- CatatanBolongPuasa/script.js
+++ CatatanBolongPuasa/script.js
@@ -18,7 +18,13 @@ function hideAdd() {
 }
 
 function Delete(index){
-    database.pop(index)
+    data = []
+    database.forEach((element,i) => {
+        if (index != i) {
+            data.push(element)
+        }
+    })
+    database = data
     DrawData(database)
     save()
 }
@@ -39,14 +45,31 @@ function onCheckbox(index){
     console.log(index)
 }
 
+function onALLDelete(){
+    const ck = document.querySelectorAll("#select")
+    ck.forEach((element) => {
+        if (element.checked) {
+            data = []
+            database.forEach((e,i)=>{
+                if (i != parseInt(element.getAttribute("index"))) {
+                    data.push(e)
+                }
+            })
+            database = data
+        }
+    })
+    DrawData(database)
+    save()
+}
+
 const root = document.getElementById("root")
 function DrawData(data){
     let element_html = "";
     data.forEach((element , index) => {
         element_html = `
-                    <div class="min-h-20 flex  ${(index != data.length - 1) ? "border-b-neutral-700 border-b-2" : ""}">
+                    <div class="min-h-20 flex hover:bg-neutral-700  ${(index != 0) ? "border-b-neutral-700 border-b-2" : ""}">
                         <div class="w-20 justify-center h-20 flex items-center">
-                            <input id="select" onchange="onCheckbox(${index})" class="w-full  h-[30%] " type="checkbox" name="" id="">
+                            <input id="select" index="${index}" class="w-full  h-[30%] " type="checkbox" name="" id="">
                         </div>
                         <div class="flex-1 flex items-center">
                             <p class="text-gray-400">${element.alasan}</p>
@@ -56,7 +79,7 @@ function DrawData(data){
                             <p class="cursor-pointer  text-gray-400">${element.date}</p>
                         </div>
                         <div class="w-30 flex">
-                            <button onclick="Delete(${index})" class="cursor-pointer text-red-400">Delete</button>
+                            <button onclick="Delete(${index})" class="hover:text-red-600 cursor-pointer text-red-400">Delete</button>
                         </div>
                     </div>
         ` + element_html
